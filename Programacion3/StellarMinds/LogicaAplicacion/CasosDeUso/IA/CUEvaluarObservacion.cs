1462fe8312d960428fea48a30463f82f88a12449
FaustosuHz <114538838+FaustosuHz@users.noreply.github.com>
2026-08-29 15:23:19 -0300

    Proyectos nuevos 2

--- Programacion3/StellarMinds/LogicaAplicacion/CasosDeUso/IA/CUEvaluarObservacion.cs
+++ Programacion3/StellarMinds/LogicaAplicacion/CasosDeUso/IA/CUEvaluarObservacion.cs
@@ -95,7 +95,7 @@ namespace LogicaAplicacion.CasosUso.Observaciones
                 "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
             );
 
-            request.Headers.Add("X-goog-api-key", "poner apikey aca");
+            //apikey
 
             request.Content = new StringContent(
                 JsonSerializer.Serialize(requestBody),
